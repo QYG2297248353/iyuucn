a7ab9d40e222e79b60797ff6dac9ffe2466d6bb6
david <nz1985@qq.com>
2024-03-25 20:20:45 +0800

    composer update

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -23,9 +23,9 @@ class IndexController
         // 今日新增用户数
         $today_user_count = User::where('created_at', '>', date('Y-m-d') . ' 00:00:00')->count();
         // 7天内新增用户数
-        $day7_user_count = User::where('created_at', '>', date('Y-m-d H:i:s', time() - 7 * 24 * 60 * 60))->count();
+        $day7_user_count = User::where('created_at', '>', date('Y-m-d H:i:s', time() - 7 * 86400))->count();
         // 30天内新增用户数
-        $day30_user_count = User::where('created_at', '>', date('Y-m-d H:i:s', time() - 30 * 24 * 60 * 60))->count();
+        $day30_user_count = User::where('created_at', '>', date('Y-m-d H:i:s', time() - 30 * 86400))->count();
 
         return view('index/index', [
             'app_key' => config('plugin.ledc.push.app.app_key'),
