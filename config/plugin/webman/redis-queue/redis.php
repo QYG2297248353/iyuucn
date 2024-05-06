34154135a78f658a899fc1e315cf4cf52379431b
萌森 <qyg2297248353@163.com>
2024-05-06 13:55:26 +0800

    修改 Redis 连接配置

--- config/plugin/webman/redis-queue/redis.php
+++ config/plugin/webman/redis-queue/redis.php
@@ -2,9 +2,9 @@
 
 return [
     'default' => [
-        'host' => 'redis://127.0.0.1:6379',
+        'host' => 'redis://' . env('REDIS_HOST', '127.0.0.1') . ':' . env('REDIS_PORT', 6379),
         'options' => [
-            'auth' => null,       // 密码，字符串类型，可选参数
+            'auth' => env('REDIS_AUTH', null),       // 密码，字符串类型，可选参数
             'db' => env('REDIS_DB', 2),            // 数据库
             'prefix' => '',       // key 前缀
             'max_attempts' => 2, // 消费失败后，重试次数
